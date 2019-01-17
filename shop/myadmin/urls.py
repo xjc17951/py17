fbb8c8d54b375013faa043f79041bef94aa0dba2
xjc <1795134863@qq.com>
2019-01-17 20:48:42 +0800

    用户数据的增删改查及分页搜索

--- shop/myadmin/urls.py
+++ shop/myadmin/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
 	url(r'^$',views.index,name="myadmin_index"),
 	url(r'^vipuser$',views.vipuser,name="myadmin_vipuser"),
 	url(r'^adduser$',views.adduser,name="myadmin_adduser"),
+	url(r'^deluser/$', views.deluser,name='myadmin_deluser'),
+	url(r'^edituser/$', views.edituser,name='myadmin_edituser'),
+	# 重置密码
+	url(r'^respwd/$', views.respwd,name='myadmin_respwd'),
+	# 修改状态
+	url(r'^changes/$', views.changes,name='myadmin_changes'),
 ]
